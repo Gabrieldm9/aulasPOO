493e995cb5c5435053215e4a3c4479bd2fee9bd7
Gabriel Demmer Maciel <gabrieldm9@gmail.com>
2018-09-18 09:11:10 -0300

    mais alguns testes

--- agenda/src/test/java/poo/TelefoneTest.java
+++ agenda/src/test/java/poo/TelefoneTest.java
@@ -12,10 +12,13 @@ public class TelefoneTest {
 
         Telefone telefone = new Telefone();
 
-        Assert.assertEquals("XXXXXXX não é um número válido", "número inválido", telefone.add("casa", "XXXXXXXXX") );
-        Assert.assertEquals("456 não é um número válido", "número inválido", telefone.add("fixo", "456") );
-        Assert.assertEquals("telefone só deve conter números","número inválido",telefone.add("celular", "(49)98848-1595") );
-
+        Assert.assertEquals("rótulo e telefone não podem ser nulos",false,telefone.add(" "," "));
+        Assert.assertEquals("telefone não pode ser nulo",false,telefone.add("fixo"," "));
+        Assert.assertEquals("rótulo não pode ser nulo",false,telefone.add(" ","48984545665"));
+        Assert.assertEquals("XXXXXXX não é um número válido", false, telefone.add("casa", "XXXXXXXXX") );
+        Assert.assertEquals("456 não é um número válido", false, telefone.add("fixo", "456") );
+        Assert.assertEquals("telefone só deve conter números",false,telefone.add("celular", "(49)98848-1595") );
+        Assert.assertEquals("o número de telefone deve conter 11 caracteres", false, telefone.add("celular","489885025959"));
 
 
 
@@ -24,13 +27,27 @@ public class TelefoneTest {
 
     @Test
     public void testaRemove() {
+
+        Telefone telefone = new Telefone();
+
+
+        Assert.assertEquals("rótulo nulo é inválido",false,telefone.remove(" "));
+
+
+
+
+
+
     }
 
     @Test
     public void testaUpdate() {
+
+
+        
+
+
     }
 
-    @Test
-    public void testaToString() {
-    }
+
 }
